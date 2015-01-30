50d416384536e0e73d43ac6ca8bb3909eac70f51
kocsen <kocsenc@gmail.com>
2015-01-30 11:17:16 -0500

    Update docs and targets

--- class-prep-tools/ParseNamesFromMycourses.py
+++ class-prep-tools/ParseNamesFromMycourses.py
@@ -1,34 +1,37 @@
+"""
+Author: Kocsen Chung
 
+Parses raw copy of the MyCourses classlist.
+(Ctrl-C on highlighting the classlist)
+
+TODO: Will be an HTML parser of the entire classlist mycourses page
+"""
 
 
 def main():
-	lines = []
-	with open('students.txt') as f:
-		lines = f.readlines()
+    with open('students.txt') as f:
+        lines = f.readlines()
 
-	parsed_lines = []
-	for line in lines:
-		parsed = line.strip().split('\t')
-		del parsed[1]
-		parsed_lines.append(parsed)
+    parsed_lines = []
+    for line in lines:
+        parsed = line.strip().split('\t')
+        del parsed[1]
+        parsed_lines.append(parsed)
 
-	print parsed_lines
-	with open('out.txt', 'w+') as f:
-		for line in parsed_lines:
-			name = line[0]
-			split_name = name.split(' ')
+    with open('out.txt', 'w+') as f:
+        for line in parsed_lines:
+            name = line[0]
+            split_name = name.split(' ')
 
-			f.write(split_name[1])
-			f.write('\t')
-			f.write(split_name[0])
-			f.write('\t')
-			f.write(line[1])
-			f.write('\n')
+            f.write(split_name[1])
+            f.write('\t')
+            f.write(split_name[0])
+            f.write('\t')
+            f.write(line[1])
+            f.write('\n')
 
 
-    	
-
-main()
-
+if __name__ == "__main__":
+    main()
 
 
