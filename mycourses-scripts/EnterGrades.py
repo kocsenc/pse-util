1c32df4ea0bf676f4d3ca128cdf4d7ba4bae1370
kocsen <kocsenc@gmail.com>
2015-02-13 11:48:40 -0500

    Added feedback functionality

--- mycourses-scripts/EnterGrades.py
+++ mycourses-scripts/EnterGrades.py
@@ -7,6 +7,12 @@ Given a list of grades in order, will insert them into
 mycourses Grade Entry page.
 """
 from selenium import webdriver
+import sys
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 GRADES_FILE = "grades.txt"
 
@@ -74,21 +80,47 @@ def enter_grades_mycourses(grades):
     :return:
     """
     d = webdriver.Firefox()
+    w = WebDriverWait(d, 10) # Waiting mechanism
+
     d.get("http://www.mycourses.rit.edu")
     input("Please log in and navigate to grade entry page and press Enter")
 
     grade_textboxes = d.find_element_by_id("z_p").find_elements_by_class_name("d_edt")
 
-    # TODO: add check if textbox# == grade #
+    # check if textbox size == grade size
+    if len(grade_textboxes) != len(grades):
+        d.close()
+        sys.exit()
 
+    # Enter Grades
     for i in range(len(grade_textboxes)):
         textbox = grade_textboxes[i]
         grade = grades[i]
         textbox.send_keys(grade)
         time.sleep(0.3)
 
+    # Enter feedback
+    for i in range(len(grade_textboxes)):
+        xpath = FeedbackXpath(i + 5).xpath
+        feedback_button = d.find_element_by_xpath(xpath)
+        feedback_button.click()
+        try:
+            # MyCourses still wont recognize this id
+            feedback_area = w.until(EC.presence_of_element_located((By.ID, "tinymce")))
+        finally:
+            print("oh oh")
+            sys.exit()
+
     input("Grades should be inputted, revise and submit")
     d.close()
+
+
+class FeedbackXpath:
+    def __init__(self, elm):
+        self.elm = str(elm)
+        self.part_one = '//*[@id="z_p"]/tbody/tr['
+        self.part_two = ']/td[6]/a'
+        self.xpath = self.part_one + self.elm + self.part_two
 
 
 if __name__ == "__main__":
