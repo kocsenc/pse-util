4467f45599cbabc39005c4a6039ad9aeea036d61
Kocsen <kocsenc@gmail.com>
2015-09-01 10:33:22 -0400

    Updated to be more robust

--- class-prep-tools/ParseNamesFromMycourses.py
+++ class-prep-tools/ParseNamesFromMycourses.py
@@ -2,7 +2,10 @@
 Author: Kocsen Chung
 
 Parses raw copy of the MyCourses classlist.
-(Ctrl-C on highlighting the classlist)
+
+To use:
+    Navigate to Classlist > (top-right) Print > Select Students Tab > Higlight table body and copy to clipboard
+    Save the contents of the clipboard to a file `students.txt` and run this script, expect an out.txt
 
 TODO: Will be an HTML parser of the entire classlist mycourses page
 """
@@ -15,20 +18,34 @@ def main():
     parsed_lines = []
     for line in lines:
         parsed = line.strip().split('\t')
-        del parsed[1]
         parsed_lines.append(parsed)
 
     with open('out.txt', 'w+') as f:
         for line in parsed_lines:
-            name = line[0]
-            split_name = name.split(' ')
+            uid = line[1]
+            first, last = parse_name(line[0])
 
-            f.write(split_name[1])
+            f.write(last)
             f.write('\t')
-            f.write(split_name[0])
+            f.write(first)
             f.write('\t')
-            f.write(line[1])
+            f.write(uid)
             f.write('\n')
+
+def parse_name(raw_name):
+    ary = raw_name.split(',')
+    if 'is online' in raw_name:
+        last = ary[0]
+
+        dup_fname = ary[1].strip().split(' ')[0]
+        cut_at = int(len(dup_fname)/2)
+
+        first = dup_fname[:cut_at]
+
+        return (first,last)
+    else:
+        return (ary[0].strip(), ary[1].strip())
+
 
 
 if __name__ == "__main__":
